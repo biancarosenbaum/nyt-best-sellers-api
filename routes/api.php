6527a37b72ce24e259bd1605d7766cd8794df155
Bianca Rosenbaum <brosenbaum@chicagobotanic.org>
2022-02-15 09:07:44 -0600

    Create api route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\NytBestSellersSearchController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function () {
+    Route::get('api/1/nyt/best-sellers', NytBestSellersSearchController::class)->name('best-sellers.search');
 });
